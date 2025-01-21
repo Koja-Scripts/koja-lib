c65abfff97a1c4c76e7c2e0afefeedf8a12f7dc1
Koja-Scripts <67247408+jamnik1337@users.noreply.github.com>
2025-01-21 11:37:28 +0100

    Added export to identity player from source and identifier

---
+++ server/framework.lua
@@ -0,0 +1,114 @@
+if GetResourceState('es_extended') == 'started' then
+    
+    local ESX = exports['es_extended']:getSharedObject()
+
+    local convertMoney = {
+        ["cash"] = "money",
+        ["bank"] = "bank"
+    }
+
+    function getPlayer(src)
+        return ESX.GetPlayerFromId(src)
+    end
+
+    function getCharID(src)
+        local Player = getPlayer(src)
+        return Player.identifier
+    end
+
+    function getPlayerJob(src)
+        local Player = getPlayer(src)
+        return Player.job.name, Player.job.grade
+    end
+
+    function getMoney(src, mtype)
+        local Player = getPlayer(src)
+        if not Player then return end
+        mtype = convertMoney[mtype] or mtype
+        return Player.getAccount(mtype).money
+    end
+
+    function removeMoney(src, amount, mtype, reason)
+        local mtype = convertMoney[mtype] or mtype
+
+        local Player = getPlayer(src)
+        if not Player then
+            return false
+        end
+
+        if Player.getAccount(mtype).money < amount then
+            return false
+        end
+
+        Player.removeAccountMoney(mtype, amount, reason)
+        return true
+    end
+
+elseif GetResourceState('ox_core') == 'started' then
+
+    function getPlayer(id)
+        return Ox.GetPlayer(id)
+    end
+    
+    function getCharID(src)
+        local player = getPlayer(src)
+        return player.charId
+    end
+    
+    function getPlayerJob(src)
+        local player = getPlayer(src)
+        return player.getGroup()
+    end
+    
+    function getMoney(src, mtype)
+        if mtype == 'cash' then
+            return exports.ox_inventory:GetItemCount(src, 'money')
+        else
+            return
+        end
+    end
+    
+    function removeMoney(src, amount, mtype, reason)
+        if mtype == 'cash' then
+            return exports.ox_inventory:RemoveItem(src, 'money', amount)
+        else
+            return
+        end
+    end
+
+elseif GetResourceState('qb-core') == 'started' then
+
+    local QBCore = exports['qb-core']:GetCoreObject()
+
+    function getPlayer(src)
+        return QBCore.Functions.GetPlayer(src)
+    end
+
+    function getCharID(src)
+        local player = getPlayer(src)
+        return player.PlayerData.citizenid
+    end
+
+    function getPlayerJob(src)
+        local player = getPlayer(src)
+        return player.PlayerData.job.name, player.PlayerData.job.grade.level
+    end
+
+    function getMoney(src, mtype)
+        local player = getPlayer(src)
+        if not player then return end
+        return player.PlayerData.money[mtype]
+    end
+
+    function removeMoney(src, amount, mtype, reason)
+        local player = getPlayer(src)
+        if not player then return end
+
+        if player.PlayerData.money[mtype] < amount then
+            return
+        end
+
+        return player.Functions.RemoveMoney(mtype, amount, reason or "unknown")
+    end
+
+end

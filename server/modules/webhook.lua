a710b58bc68ad9ff1091992f9ee02d7b98e6261f
Koja-Scripts <67247408+jamnik1337@users.noreply.github.com>
2025-01-28 21:37:25 +0100

    Hotfixes

--- server/modules/webhook.lua
+++ server/modules/webhook.lua
@@ -1,5 +1,9 @@
 webhook = function(embed, hook)
-    PerformHttpRequest(KOJA.Server.Webhooks[hook], function(err, text, headers) end, 'POST', json.encode({embeds = {embed}}), {['Content-Type'] = 'application/json'})
+    if KOJA.Server.Webhooks[hook] then
+        PerformHttpRequest(KOJA.Server.Webhooks[hook], function(err, text, headers) end, 'POST', json.encode({embeds = {embed}}), {['Content-Type'] = 'application/json'})
+    else
+        print('[^2KOJA_LIB^7] Webhook to ^1'..hook..'^7 does not exist!')
+    end
 end
 
 KOJA.Server.LogMessage = function(data, hook)

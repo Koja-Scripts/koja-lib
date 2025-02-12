d321c29a954b9eccf8677553aa277c6b1cf42758
Koja-Scripts <67247408+jamnik1337@users.noreply.github.com>
2025-02-12 02:49:43 +0100

    Added Get Item Count functions in client side

---
+++ client/modules/inventory.lua
@@ -0,0 +1,26 @@
+---@return number # Liczba określonych przedmiotów w ekwipunku
+KOJA.Client.GetItemCount = function(item)
+    if KOJA.Framework == "esx" then
+        local playerData = ESX.GetPlayerData()
+        for _, inventoryItem in ipairs(playerData.inventory) do
+            if inventoryItem.name == item then
+                return inventoryItem.count
+            end
+        end
+        return 0
+    elseif KOJA.Framework == "qb" then
+        local playerData = QBCore.Functions.GetPlayerData()
+        local count = 0
+        for _, inventoryItem in pairs(playerData.items) do
+            if inventoryItem.name == item then
+                count = inventoryItem.amount
+                break
+            end
+        end
+        return count
+    elseif KOJA.Framework == "ox" then
+        return exports.ox_inventory:GetItemCount(item)
+    else
+        return 0
+    end
+end

bfe94a46c6b2e224862e12d91b7ec813f25ff0e7
Koja-Scripts <67247408+jamnik1337@users.noreply.github.com>
2025-02-20 17:09:41 +0100

    Update framework.lua

--- server/framework.lua
+++ server/framework.lua
@@ -37,7 +37,7 @@ if not Config.CustomFramework then
             mtype = convertMoney[mtype] or mtype
             local account = Player.getAccount(mtype)
             if not account then print('[ESX] Account missed, add ESX accounts for the script to work properly') end
-            print(Player.getAccount(mtype))
+            print(json.encode(Player.getAccount(mtype)))
             return Player.getAccount(mtype).money
         end
 
